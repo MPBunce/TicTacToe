23e971dd51a8317abb44cdf6899ccee3b4f9ee18
Matthew Bunce <matthew-bunce@outlook.com>
2023-03-14 17:56:13 -0400

    nice

--- tictactoe-cpp/main.cpp
+++ tictactoe-cpp/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
 
+//Functions
+void printBoard( char board[100][100] ){
+
+    int x = 3;
+    
+    for(int i; i < x; ++i){
+        cout << board[i];
+    }
+
+}
+//Main
+
 int main(){
 
-
+    char gameBoard[3][3] = {
+        {'1', '2', '3',},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
     printf("\nhello\n\n");
+
+    printBoard( gameBoard );
+
     return(0);
 
 
 }
+
